ed5204f9539078b0727736c6c191930349198529
Salim Çobanoğlu <salimcobanoglu@yahoo.com>
2023-03-02 16:59:02 +0100

    son kaldı

--- index.js
+++ index.js
@@ -119,34 +119,69 @@ console.log("En Büyük Sayı: ", enkucuk)
 	//(.forEach metodunu kullanın)
 	// 3b çözümü:
 
-	/* kodlar buraya */
-		
-		
-		
+	
+	
+	  var ucetambolunenler = [];
+
+	  sayilar.forEach((x) => {
+		if (x % 3 === 0) { 
+			ucetambolunenler.push(x)}
+		})   
+		  console.log(ucetambolunenler)
+
+  
+
+
+	//3c. `ucetambolunenler` dizisindeki sayıların toplamını .reduce metoduyla bulup, sonucu `ucebolunenlerintoplami` değişkenine yazdırın 
+	//(.reduce metodunu kullanın)
 	//3c çözümü:
 	
-	/* kodlar buraya */
+	var ucebolunenlerintoplami = ucetambolunenler.reduce((onceki,mevcut) => {return onceki + mevcut},0)
+	  console.log(ucebolunenlerintoplami)
+
 
 	
-	
+	//3d. `besyuzdenkucuksayilar` adında bir dizi oluşturarak, sayilar dizisinin içindeki 500'den küçük sayıları bu diziye atayın 
+	//(.filter metodunu kullanın)
 	//3d çözümü
-	
-	/* kodlar buraya */
+
+	var besyuzdenkucuksayilar = sayilar.filter((x) => x < 500);
+  console.log(besyuzdenkucuksayilar)
 
 
 
+
+	//3e. besyuzdenkucuksayilar dizisindeki sayıları küçükten büyüğe sıralayıp `siralisayilar` adındaki bir diziye aktarın 
+	//(.sort metodunu kullanın)
 	//3e çözümü
 
-	/* kodlar buraya */
+	var siralisayilar = besyuzdenkucuksayilar.sort((a, b) => a - b);
+console.log(siralisayilar); 
 	
-	
+	/*
+	3f. `tekraredensayilar` adında bir dizi oluşturun. sayilar dizisi içerisindeki bazı sayılar birden fazla kere yazılmış. 
+	sayilar dizisi içerisinde birden fazla kez yazılmış sayıları tespit ederek kaç kere tekrar edildiğini belirten bir string oluşturulup 
+	`tekraredensayilar` dizisine aktarılmasını istiyoruz. Örnek string: "{sayı} sayısı {tekrarSayisi} kere tekrar edilmiştir"
+	ÖRNEK: sayilar dizisi içerisinde 45 sayısı 3 kere yazılmış. "45 sayısı 3 tekrar edilmiştir" stringini `tekraredensayilar` 
+	dizisine aktaracağız.
+	💡 İPUCU: Tekrar edilen sayıları ve kaç kere tekrar edildiğini kaydetmek için bir nesne tanımlamalısınız, bu görevi yapabilmek 
+	için en az 2 kere döngü yazmalısınız. Birinci döngüde hangi sayının kaç kere tekrar edildiğini tespit edip, 2. döngüde stringi 
+	oluşturup verilen diziye aktarmalısınız.
+	*/
 	//3f çözümü
 	
-	/* kodlar buraya */
+	const tekrarEdenSayilar = tekraredenlerBurada(sayilar);
 
-
-
-
+	function tekraredenlerBurada(sayilarP){
+	const tekraredenler = []
+	 for (let i=0; i<sayilarP.length; i++) {
+	   if (sayilarP[i] == sayilarP[i+1]) {  
+		 tekraredenler.push(sayilarP[i])
+	   }
+	 }
+	  return tekraredenler;
+	}
+	console.log(tekraredenlerBurada(sayilar))
 	
 		
 
